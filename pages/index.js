fc310844f9db0fbfbb19fbc8b68700d064d34bfd
Yavuz Selim <yavuzselim.s584@gmail.com>
2024-10-13 21:43:50 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -37,10 +37,10 @@ export default function Home() {
         <section className="h-full max-w-5xl flex items-center justify-center">
           <div className="flex items-center justify-center flex-col">
             <h4 className="text-lg text-white">
-              <span className="text-xl font-normal">Hello,</span> I'm
+              <span className="text-xl font-normal">Merhaba,</span> Ben
             </h4>
             <h1 className="mt-5 text-xl font-extrabold tracking-normal uppercase md:text-4xl md:mt-3 text-white">
-              Erdem Doruk
+              Yavuz Selim Sarıhan
             </h1>
 
             <div className="mt-5 text-2xl font-normal text-transparent bg-gradient-to-r from-pink-500 via-indigo-400 to-purple-700 bg-clip-text md:mt-3">
@@ -50,7 +50,7 @@ export default function Home() {
             {/* Social Accounts */}
             <div className="flex items-center mt-8 space-x-6 md:mt-4">
               <motion.a
-                href="https://github.com/realerdemdoruk"
+                href="https://github.com/Selexis"
                 target={"_blank"}
                 rel="noopener noreferrer"
                 className="flex items-center justify-center  transition-colors duration-300 text-gray-100 hover:text-pinkish-200"
@@ -59,18 +59,10 @@ export default function Home() {
                 <GithubIcon className={"w-8 h-8 fill-current"} />
               </motion.a>
 
-              <motion.a
-                href="https://twitter.com/realerdemdoruk"
-                target={"_blank"}
-                rel="noopener noreferrer"
-                className="flex items-center justify-center  transition-colors duration-300 text-gray-100 hover:text-pinkish-200"
-                whileHover={{ scale: 1.1 }}
-              >
-                <TwitterIcon className={"w-8 h-8 fill-current"} />
-              </motion.a>
+             
 
               <motion.a
-                href="https://www.linkedin.com/in/erdem-doruk-2954b7227/"
+                href="https://www.linkedin.com/in/yavuz-selim-sar%C4%B1han-8423432bb/"
                 target={"_blank"}
                 rel="noopener noreferrer"
                 className="flex items-center justify-center  transition-colors duration-300 text-gray-100 hover:text-pinkish-200"
